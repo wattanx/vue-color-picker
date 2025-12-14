da791353bd681dd9160b5ac0ea7fee7be5d5792a
Ryota Watanabe <43837308+wattanx@users.noreply.github.com>
2025-12-14 12:17:37 +0900

    feat: support touch event (#27)
    * feat: support touch event
    
    * fix: add cleanup

--- packages/vue-color-picker/src/components/ColorPickerCanvas.ts
+++ packages/vue-color-picker/src/components/ColorPickerCanvas.ts
@@ -1,4 +1,4 @@
-import { defineComponent, ref, h } from "vue";
+import { defineComponent, ref, h, onUnmounted } from "vue";
 import type { Position } from "../types/position";
 import { clamp } from "../utils";
 
@@ -8,8 +8,13 @@ export const ColorPickerCanvas = defineComponent({
   },
   setup(_, { emit, slots }) {
     const divRef = ref<HTMLDivElement | null>(null);
+    const cleanupFns: Array<() => void> = [];
 
-    const move = (e: MouseEvent): void => {
+    onUnmounted(() => {
+      cleanupFns.forEach((fn) => fn());
+    });
+
+    const move = (e: MouseEvent | Touch): void => {
       if (divRef.value) {
         const { width, height, left, top } =
           divRef.value.getBoundingClientRect();
@@ -30,15 +35,54 @@ export const ColorPickerCanvas = defineComponent({
         move(_e);
       };
 
-      const onMouseUp = (_e: MouseEvent): void => {
+      const cleanup = (): void => {
         document.removeEventListener("mousemove", onMouseMove, false);
         document.removeEventListener("mouseup", onMouseUp, false);
+        const index = cleanupFns.indexOf(cleanup);
+        if (index > -1) {
+          cleanupFns.splice(index, 1);
+        }
+      };
 
+      const onMouseUp = (_e: MouseEvent): void => {
+        cleanup();
         move(_e);
       };
 
+      cleanupFns.push(cleanup);
       document.addEventListener("mousemove", onMouseMove, false);
       document.addEventListener("mouseup", onMouseUp, false);
+    };
+
+    const onTouchStart = (e: TouchEvent): void => {
+      if (e.touches.length !== 1) return;
+
+      e.preventDefault();
+      move(e.touches[0]);
+
+      const onTouchMove = (_e: TouchEvent): void => {
+        move(_e.touches[0]);
+      };
+
+      const cleanup = (): void => {
+        document.removeEventListener("touchmove", onTouchMove, false);
+        document.removeEventListener("touchend", onTouchEnd, false);
+        const index = cleanupFns.indexOf(cleanup);
+        if (index > -1) {
+          cleanupFns.splice(index, 1);
+        }
+      };
+
+      const onTouchEnd = (_e: TouchEvent): void => {
+        cleanup();
+        if (_e.changedTouches.length > 0) {
+          move(_e.changedTouches[0]);
+        }
+      };
+
+      cleanupFns.push(cleanup);
+      document.addEventListener("touchmove", onTouchMove, false);
+      document.addEventListener("touchend", onTouchEnd, false);
     };
 
     return () =>
@@ -46,6 +90,7 @@ export const ColorPickerCanvas = defineComponent({
         "div",
         {
           onMousedown,
+          onTouchstart: onTouchStart,
           ref: divRef,
         },
         slots.default?.()
